23e0dea50f7ef41a34165793ba5a51ff28136ea1
guop8 <guop85@gmail.com>
2024-05-26 18:31:18 +0300

    huggywuggy

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+
+    public Image fade;
+
+    [Header("Game Over UI")]
+    public GameObject gameOver;
+    public Text gameOverScore;
+
+    [Header("New Wave UI")]
+    public RectTransform newWaveBanner;
+    public Text newWaveTitle;
+    public Text newWaveEnemiesCount;
+
+    [Header("Health Bar & Score")]
+    public Text score;
+    public RectTransform healthBar;
+
+    Player player;
+
+    //to access each new wave
+    Spawner spawner;
+
+
+
+    private void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        player.OnDeath += OnGameOver;
+    }
+
+
+    private void Update()
+    {
+        score.text = "очки: " + ScoreKeeper.score.ToString("D6");
+
+        float healthPercent = 0;
+        if (player != null)
+        {
+            healthPercent = player.health / player.startingHealth;
+        }
+        healthBar.localScale = new Vector3(healthPercent, 1, 1);
+    }
+
+
+    public void OnNewWave(int waveNumber) {
+        string[] numbers = { "Первая", "Вторая", "Третья", "Четвертая", "Пятая" };
+        newWaveTitle.text = "- Волна " + numbers[waveNumber - 1] + " -";
+
+        string enemyCount = ((spawner.waves[waveNumber - 1].infinite) ? "Бесконечно" : spawner.waves[waveNumber - 1].enemyCount + "");
+        /*
+        if (spawner.waves[waveNumber - 1].infinite)
+        {
+            enemyCount = "Infinite";
+        } else
+        {
+            spawner.waves[waveNumber - 1].enemyCount;
+        }
+        */
+        newWaveEnemiesCount.text = "Врагов: " + enemyCount;
+
+        StopCoroutine("AnimateBanner");
+        StartCoroutine("AnimateBanner");
+    }
+
+
+    IEnumerator AnimateBanner() {
+        float percent = 0;
+        float speed = 2.5f;
+        int dir = 1;
+        float delayTime = 1f;
+        
+
+        while (percent >= 0) {
+            percent += Time.deltaTime * (1 / speed) * dir;
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-5000, 40, percent);
+
+            if (percent > 1) {
+                dir = -1;
+                yield return new WaitForSeconds(delayTime);
+            }
+
+            yield return null;
+        }
+    }
+
+
+    void OnGameOver() {
+        StartCoroutine(Fade(Color.clear, new Color(0, 0, 0, 0.95f), 1));
+        score.gameObject.SetActive(false);
+        healthBar.gameObject.SetActive(false);
+        gameOverScore.text = score.text;
+        gameOver.SetActive(true);
+    }
+
+    IEnumerator Fade(Color from, Color to, float second) {
+        float speed = 1 / second;
+        float percent = 0;
+
+        while (percent < 1) {
+            percent += Time.deltaTime * speed;
+            fade.color = Color.Lerp(from, to, percent);
+            yield return null;
+        }
+    }
+
+
+    public void PlayAgain() {
+
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (PlayerController))]
+[RequireComponent(typeof(GunController))]
+public class Player : LivingEntity
+{
+    public float moveSpeed = 5f;
+    PlayerController controller;
+    GunController gunController;
+
+    Camera viewCamera;
+
+    public Crosshairs crosshair;
+
+
+    private void Awake()
+    {
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
+        viewCamera = Camera.main;
+        FindObjectOfType<Spawner>().OnNewWave += OnNewWave;
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start(); // run Start() in `LivingEntity` first, then this Start()
+    }
+
+
+    void OnNewWave(int waveNumber) {
+        health = startingHealth;
+        gunController.EquipGun(waveNumber - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 velocity = direction.normalized * moveSpeed;
+        controller.Move(velocity);
+
+
+        // Look at mouse position
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.forward * gunController.GunHeight);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin, point, Color.red);
+
+            controller.LookAt(point);
+
+            crosshair.transform.position = point;
+            crosshair.DetectTarget(ray);
+
+            if ((new Vector2(point.x, point.z) - new Vector2(transform.position.x, transform.position.z)).sqrMagnitude > 1)
+            {
+                gunController.Aim(point);
+            }
+            
+        }
+
+
+        // Weapon shoots
+        if (Input.GetMouseButton(0))
+        {
+            gunController.OnTriggerHold();
+        }
+
+        // Stop shooting
+        if (Input.GetMouseButtonUp(0))
+        {
+            gunController.OnTriggerReleased();
+        }
+
+
+        //Reload the gun
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gunController.Reload();
+        }
+
+        // GOD mode, auto shoot, no need for holding mouse button
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            StartCoroutine(AutoShoot());
+        }
+
+        if(transform.position.y < -5)
+        {
+            TakeDamage(health);
+        }
+    }
+
+    IEnumerator AutoShoot() {
+        while (true)
+        {
+            gunController.OnAutoShoot();
+            yield return null;
+        }
+    }
+
+    public override void Die()
+    {
+        AudioManager.instance.PlaySound("Player Death", transform.position);
+        base.Die();
+    }
+}
